8d87dec8ec4fc67e5a942626b7525c3c4b9122f8
b-s-x <b.s.x.mars@gmail.com>
2020-06-16 20:35:32 +0300

    global directory change

--- src/js/index.js
+++ src/js/index.js
@@ -1,3 +1,5 @@
+import Counter from './counter';
+
 const countButtonOne = document.querySelector('#countOne')
 const countButtonTwo = document.querySelector('#countTwo')
 const resetButton = document.querySelector('.reset-btn')
@@ -5,54 +7,6 @@ const scoreOne = document.querySelector('#part-score-one')
 const scoreTwo = document.querySelector('#part-score-two')
 const circleOne = document.querySelector('#circle-one')
 const circleTwo = document.querySelector('#circle-two')
-
-class Counter {
-  constructor () {
-    this.data = 0;
-    this.score = 0;
-  }
-
-  setCount() { return this.data++ }
-
-  allReset() {
-    this.data = 0
-    this.score = 0
-  }
-
-  locReset() { return this.data = 0 }
-
-  sub() { return this.data-- }
-
-  setScore() { return this.score++ }
-
-  getData() { return this.data }
-
-  getScore() { return this.score }
-
-  debounce (func, wait) {
-    let timeout;
-
-    return function exeFunc() {
-      const later = () => {
-        timeout = null;
-        func.apply(this, arguments);
-
-      }
-      clearTimeout(timeout);
-      timeout = setTimeout(later, wait);
-    }
-  }
-
-  resetViewCount(buttonCounter, scoreCounter) {
-    buttonCounter.innerHTML = 0;
-    scoreCounter.innerHTML = 0;
-  }
-
-  refresh(newCounter, buttonCounter, scoreCounter) {
-    buttonCounter.innerHTML = newCounter.getData()
-    scoreCounter.innerHTML = newCounter.getScore()
-  }
-}
 
 const leftCounter = new Counter();
 const rightCounter = new Counter();
