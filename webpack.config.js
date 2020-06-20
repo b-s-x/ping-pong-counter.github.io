3351564e5ea1117b6ff4c0b1b4b923f38cb76b0d
b-s-x <b.s.x.mars@gmail.com>
2020-06-20 16:43:29 +0300

    fix webpack, add plugins Html-plugin, clean-plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,19 @@
 const path = require('path');
+const HTMLWebpackPlugin = require('html-webpack-plugin');
+const {CleanWebpackPlugin} = require('clean-webpack-plugin');
 
 module.exports = {
-  entry: './src/js/index.js',
+  entry: path.resolve(__dirname, './src/js/index.js'),
   output: {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist'),
   },
+  plugins: [
+    new HTMLWebpackPlugin({
+      template: 'index.html'
+    }),
+    new CleanWebpackPlugin( )
+  ],
   module: {
     rules: [
       {
